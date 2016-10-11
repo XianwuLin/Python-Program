34e42dc9c8c6d3a927b27d22179fe659f578488e
Xianwu Lin <linxianwusx@gmail.com>
2016-10-11 18:15:04 +0800

    扩展了OrderedDict
    扩展了OrderedDict对象，提供了dumps和loads方法，可以直接解析和打包有序的json格式。

---
+++ orderDict.py
@@ -0,0 +1,24 @@
+from collections import OrderedDict
+import json
+
+class OrderDict(OrderedDict):
+    def __init__(*args, **kwds):
+        OrderedDict.__init__(*args, **kwds)
+
+    def dumps(self):
+        return '{"data":%s, "index":%s}' % (json.dumps(self), json.dumps(self.keys()))
+
+    def loads(self, obj):
+        if isinstance(obj, str) or isinstance(obj, unicode):
+            str_json = json.loads(obj)
+        elif isinstance(obj, dict):
+            str_json = obj
+            if 'data' not in str_json or 'index' not in str_json or \
+                not isinstance(str_json['data'], dict):
+                raise ValueError(u"%s is not a vaild OrderDict dumps string or dict" % unicode(obj))
+        else:
+            raise ValueError(u"%s is not a vaild OrderDict dumps string or dict" % unicode(obj))
+
+        for item in str_json['index']:
+            self[item] = str_json['data'].get(item)
+        return self
