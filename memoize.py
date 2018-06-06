9abe0a3a84a837f863db8a4eb58c913cdba9f6f1
Xianwu Lin <linxianwusx@gmail.com>
2018-06-06 17:36:33 +0800

    Update memoize.py

--- memoize.py
+++ memoize.py
@@ -4,60 +4,47 @@
 """
 这里构造了一个缓存函数的装饰器，可以缓存一个函数的调用结果，可以指定过期时间
 参考资料：http://book.pythontips.com/en/testing/function_caching.html
-
 memozie 最好传递一个unique_name参数，指定缓存函数的名称，否则不同的函数的缓存结果容易混在一起。
 """
 
 import functools
-from unqlite import UnQLite
 import time
-import json
-import pickle
+import os
 
-mem_cache_db = UnQLite()
+
+memo = {}
+
 def memoize(function=None, unique_name="unique", timeout=0):
     if not function:
         return functools.partial(memoize, unique_name=unique_name, timeout=timeout)
     @functools.wraps(function)
     def wrapper(*args, **kwargs):
         wargs = str(args) + str(kwargs)
-        now = time.time()
-        if mem_cache_db.exists(unique_name+wargs): # 已缓存
-            data = json.loads(mem_cache_db[unique_name+wargs])
-            if not timeout: #timeout=0 永不过期
-                return pickle.loads(data["result"])
+        if unique_name not in memo:
+            memo[unique_name] = dict()
+        if wargs in memo[unique_name]:
+            if timeout == 0:  #timeout=0 永不过期
+                return memo[unique_name][wargs]["result"]
             else:
-                if now - data["time"] < timeout: #未过期
-                    return pickle.loads(data["result"])
-                else: #消极过期
-                    data = function(*args, **kwargs)
-                    load_data = {"time": now, "result": pickle.dumps(data)}
-                    mem_cache_db[unique_name+wargs] = json.dumps(load_data)
-                return data
-        else: # 未缓存
-            data = function(*args, **kwargs)
-            load_data = {"time": now, "result": pickle.dumps(data)}
-            mem_cache_db[unique_name+wargs] = json.dumps(load_data)
-            return data
+                if time.time() - memo[unique_name][wargs]["time"] < timeout: #未过期
+                    return memo[unique_name][wargs]["result"]
+        rv = function(*args, **kwargs)
+        memo[unique_name][wargs] = {"time":time.time(),  #缓存的时间戳
+                            "result": rv}   #缓存的结果
+        return rv
     return wrapper
 
 
 def remove_memoize(unique_name):
     """清除缓存"""
-    keys_list = list()
-    for key in mem_cache_db.keys():
-        if key.startswith(unique_name):
-            keys_list.append(key)
-    for key in keys_list:
-        mem_cache_db.delete(key)
+    if unique_name in memo:
+        del memo[unique_name]
 
 
-@memoize(unique_name="unique", timeout=2)
-def fibonacci(n):
-    if n < 2: return n
-    return fibonacci(n - 1) + fibonacci(n - 2)
+@memoize(unique_name="unique", timeout=100)
+def set_a(num):
+    return ''.join(map(lambda xx:(hex(ord(xx))[2:]),os.urandom(10000)))
 
-
-old = time.time()
-print fibonacci(n=30)
-print time.time() - old
+def test():
+    for i in xrange(500):
+        set_a(i)
